7e488d30406d63ccf93e98439f5ba299bdf87a4b
Alexeev Yaroslav <ice-t95@mail.ru>
2018-12-07 22:48:11 +0300

    Some changes (bad commit ahaha)

---
+++ src/main/java/testSystem/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package testSystem.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
